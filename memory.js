5bf14eb78e857c1c40d7aa5e72205f3dd3e6616b
shubhamkhunt04 <shubhamkhunt08@gmail.com>
2020-10-01 10:43:21 +0530

    added readme for better understanding

--- memory.js
+++ memory.js
@@ -1,56 +1,54 @@
 const cards = document.querySelectorAll(".memoryCard");
 var isFlipped = false;
-var firstCard,secondCard;
+var firstCard, secondCard;
 var lock = false;
 var counter = 0;
-cards.forEach(card => card.addEventListener("click",flip));
+cards.forEach((card) => card.addEventListener("click", flip));
 
-function flip(){
-    if(lock) return;
-    if(this === firstCard) return;
-    this.classList.add("flip");
-    if(!isFlipped){
-        isFlipped = true;
-        firstCard = this;
-        return;
-    }
-    secondCard = this;
-    check();
+function flip() {
+  if (lock) return;
+  if (this === firstCard) return;
+  this.classList.add("flip");
+  if (!isFlipped) {
+    isFlipped = true;
+    firstCard = this;
+    return;
+  }
+  secondCard = this;
+  check();
 }
 
-function check(){
-    var isMatch = firstCard.dataset.image === secondCard.dataset.image
-    if(isMatch){
-        success();
-    }else{
-        fail();
-    }
-    //isMatch ? success() : fail();
-
+function check() {
+  var isMatch = firstCard.dataset.image === secondCard.dataset.image;
+  if (isMatch) {
+    success();
+  } else {
+    fail();
+  }
+  //isMatch ? success() : fail();
 }
-function success(){
+function success() {
+  reset();
+  firstCard.removeEventListner("click", flip);
+  secondCard.removeEventListner("click", flip);
+}
+
+function fail() {
+  lock = true;
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
     reset();
-    firstCard.removeEventListner("click",flip);
-    secondCard.removeEventListner("click",flip);
+  }, 1000);
 }
 
-function fail(){
-    lock = true;
-    setTimeout(() => {
-        firstCard.classList.remove("flip");
-        secondCard.classList.remove("flip");
-        reset();
-    }, 1000);
+function reset() {
+  [isFlipped, lock] = [false, false];
+  [firstCard, secondCard] = [null, null];
 }
-
-function reset(){
-    [isFlipped, lock] = [false,false];
-    [firstCard,secondCard] = [null,null];
-}
-(function shuffle(){
-    cards.forEach(card => {
-        var position = Math.floor(Math.random() * 16);
-        card.style.order = position;
-    });
+(function shuffle() {
+  cards.forEach((card) => {
+    var position = Math.floor(Math.random() * 16);
+    card.style.order = position;
+  });
 })();
-
